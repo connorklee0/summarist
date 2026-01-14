f8a92df7f32456da512d5578f1f07c77b33ba638
connorklee0 <connorklee0@gmail.com>
2026-01-14 14:33:14 -0600

    implement features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Nav from "@/components/Home/Nav";
 import Landing from "@/components/Home/Landing";
+import Features from "@/components/Home/Features";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Landing />
+      <Features />
     </>
   );
 }
--- components/Home/Landing.tsx
+++ components/Home/Landing.tsx
@@ -7,8 +7,8 @@ const Landing = () => {
       <div className="container">
         <div className="row">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-start">
-            <div className="w-full max-w-[560px] flex flex-col space-y-6 md:pt-6">
-              <div className="text-[#032b41] text-[40px] font-bold landing__text">
+            <div className="w-full max-w-[560px] flex flex-col text-left space-y-6 max-md:pt-6 max-md:items-center max-md:max-w-[540px] max-md:mx-auto max-md:text-center">
+              <div className="text-[#032b41] text-[40px] font-bold landing__text max-md:text-[24px]">
                 Gain more knowledge <br className="md:hidden" /> in less time
               </div>
               <div className="text-[#394547] text-[20px] font-light leading-tight landing__text">
@@ -21,7 +21,7 @@ const Landing = () => {
                 Login
               </button>
             </div>
-            <figure className="w-full h-full max-w-100">
+            <figure className="w-full h-full max-w-100 max-md:hidden">
               <Image src={LandingImg} alt="landing" />
             </figure>
           </div>
