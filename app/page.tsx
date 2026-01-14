ca3a60bd18e65fb82897a84826ccf48d80c0bbec
connorklee0 <connorklee0@gmail.com>
2026-01-14 10:41:07 -0600

    start app and implent nav and landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,56 @@
 import Image from "next/image";
+import Logo from "../public/home/logo.png";
+import Landing from "../public/home/landing.png";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <nav className="h-20">
+        <div className="flex justify-between items-center max-w-267.5 w-full h-full px-6 mx-auto">
+          <figure className="max-w-50">
+            <Image className="w-full h-full" src={Logo} alt="logo" />
+          </figure>
+          <ul className="flex gap-6">
+            <li className="text-[#032b41] transition-colors duration-100 cursor-pointer hover:text-[#2bd97c]">
+              Login
+            </li>
+            <li className="cursor-not-allowed text-[#032b41] transition-colors duration-100 hidden sm:block">
+              About
+            </li>
+            <li className="cursor-not-allowed text-[#032b41] transition-colors duration-100 hidden sm:block">
+              Contact
+            </li>
+            <li className="cursor-not-allowed text-[#032b41] transition-colors duration-100 hidden sm:block">
+              Help
+            </li>
+          </ul>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </nav>
+      <section id="landing">
+        <div className="container">
+          <div className="row">
+            <div className="flex">
+              <div className="w-full">
+                <div className="text-[#032b41] text-[40px] font-bold mb-6">
+                  Gain more knowledge <br className="md:hidden" /> in less time
+                </div>
+                <div className="text-[#394547] text-[20px] font-light mb-6 leading-normal">
+                  Great summaries for busy people, <br className="md:hidden" />
+                  individuals who barely have time to read,{" "}
+                  <br className="md:hidden" />
+                  and even people who don't like to read.
+                </div>
+                <button className="btn max-w-75 flex items-center justify-center">
+                  Login
+                </button>
+              </div>
+              <figure className="w-full h-full max-w-100">
+                <Image src={Landing} alt="landing" />
+              </figure>
+            </div>
+          </div>
         </div>
-      </main>
-    </div>
+      </section>
+    </>
   );
 }
