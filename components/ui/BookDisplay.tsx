8c7d0e76801be91934814bbddb460d429296c1ad
connorklee0 <connorklee0@gmail.com>
2026-01-28 14:35:35 -0600

    premium status books

--- components/ui/BookDisplay.tsx
+++ components/ui/BookDisplay.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { FaRegClock, FaRegStar } from "react-icons/fa6";
 import { useAudioDuration } from "@/app/hooks/useAudioDuration";
+import { useAuth } from "@/app/context/AuthContext";
 
 interface BookProps {
   id: string;
@@ -12,6 +13,7 @@ interface BookProps {
   author: string;
   subtitle: string;
   rating: string;
+  subscriptionRequired: boolean;
 }
 
 const BookDisplay = ({
@@ -22,12 +24,21 @@ const BookDisplay = ({
   author,
   subtitle,
   rating,
+  subscriptionRequired,
 }: BookProps) => {
   const duration = useAudioDuration(audio);
-  
+  const { userData } = useAuth();
+
   return (
-    <a href={`book/${id}`}>
-      <div className="flex flex-col mr-8 gap-1 p-4 hover:bg-[#c3bebe81] rounded">
+    <a href={`book/${id}`} className="w-55 ">
+      <div className="flex flex-col mr-8 gap-1 hover:bg-[#c3bebe81] rounded">
+        <div className="flex justify-end relative">
+          <div className="text-xs bg-black text-white w-15 text-center rounded-2xl absolute z-5 -top-6 right-6">
+            {subscriptionRequired &&
+              (userData?.plan === "basic" || !userData) &&
+              "Premium"}
+          </div>
+        </div>
         <div className="relative w-40 h-40 px-4">
           <Image src={image} alt={title} fill />
         </div>
