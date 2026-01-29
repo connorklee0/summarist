e43c00de3e4118ade068e11a867fe72a4361cd35
connorklee0 <connorklee0@gmail.com>
2026-01-29 14:24:52 -0600

    add route protection to sales page

--- app/choose-plan/page.tsx
+++ app/choose-plan/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Footer from "@/components/Home/Footer";
 import Banner from "@/components/Sales/Banner";
 import Dropdown from "@/components/Sales/Dropdown";
 import Plan from "@/components/Sales/PricingPlan";
+import { useRouteProtection } from "../hooks/useRouteProtection";
 
 const Sales = () => {
+  const { loading } = useRouteProtection();
+  if (loading) return null;
+
   return (
     <div className="flex flex-col">
       <Banner />
